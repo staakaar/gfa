8bf8ac4dff9b3219322c8835e5623fe1c71443a6
otobakataro <vancyroletaka@gmail.com>
2023-05-09 23:38:46 +0900

    Write: curl interactive rule

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::{io::BufReader, fs::File};
+use clap::Parser;
+
+#[derive(Parser)]
+struct Curl {
+    url: String,
+}
+
+fn main()  {
+    // icurlコマンド実行
+    // url入力
+    // オプション周りを標準出力する
+    // HTTPメソッドを選択させる
+    // POSTの場合はデータを入力させる
+    // 最後にjpを利用して最終的に実行するcurlコマンドの内容を出力
+    // 問題なければ実行　問題あれば　該当の箇所の編集ができるようにする
+
 }
