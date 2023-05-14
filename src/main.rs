fbd6e60f6b0a8b6f11e5c0bf0aa5128c4d222ef0
otobakataro <vancyroletaka@gmail.com>
2023-05-14 22:40:29 +0900

    install rustyline

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::process::{self, Command};
 use clap::Parser;
+use rustyline::Editor;
 
 #[derive(Parser)]
 struct Curl {
@@ -12,9 +13,10 @@ fn main()  {
     // icurlコマンド実行
     // url入力
     icurl_start();
-    input_url();
-    // オプション周りを標準出力する
+    let mut url = input_url();
     // HTTPメソッドを選択させる
+    input_http();
+    // オプション周りを標準出力する
     // POSTの場合はデータを入力させる
     // 最後にjpを利用して最終的に実行するcurlコマンドの内容を出力
     // 問題なければ実行　問題あれば　該当の箇所の編集ができるようにする
@@ -25,9 +27,18 @@ fn icurl_start() {
     println!("{ICURL_INPUT_URL}");
 }
 
+/// 
+/// ユーザーにURLを入力してもらうメソッド
+/// @return 
+/// 
 fn input_url() -> Result<(), std::io::Stderr> {
     let mut input = String::new();
     let url = std::io::stdin().read_line(&mut input);
 
     Ok(())
 }
+
+///
+/// HTTPメソッドをユーザーに選択させる
+///
+fn input_http() {}
