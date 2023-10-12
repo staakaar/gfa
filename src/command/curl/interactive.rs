07180b934107438440a3b67550d83e54eb750750
staakaar <vancyroletaka@gmail.com>
2023-10-12 22:59:20 +0900

    add payload impl

--- src/command/curl/interactive.rs
+++ src/command/curl/interactive.rs
@@ -28,22 +28,35 @@ impl Cmd {
         
         // ホスト名の選択
         let host_ans: Result<&str, InquireError> = Select::new("Please select an HOST name", common::curl_config::get_host()).prompt();
+        
+        let mut host_text: String = "".to_owned();
         if host_ans.unwrap() == "Other" {
-            let host_text: Result<String, InquireError> = Text::new("Please select an HOST name").prompt();
-            match host_text {
-                Ok(_) => input_host_name(),
+            let host_text_ans: Result<String, InquireError> = Text::new("Please select an HOST name").prompt();
+            match host_text_ans {
+                Ok(_) => {
+                    host_text = input_host_name();
+                },
                 Err(_) => println!("There was an error, please try again"),
             }
         }
+        println!("{}", host_text);
         
         // ポート選択
         let port_ans: Result<String, InquireError> = Text::new("Please select an PORT name").prompt();
         
         // HTTPメソッドの選択
-        let ans: Result<&str, InquireError> = Select::new("Please select an HTTP method.", common::curl_config::get_http_method()).prompt();
-        match ans {
+        let http_ans: Result<&str, InquireError> = Select::new("Please select an HTTP method.", common::curl_config::get_http_method()).prompt();
+        match http_ans {
             Ok(choice) => println!("{}! That's mine too!", choice),
             Err(_) => println!("There was an error, please try again"),
+        }
+
+        // postメソッドの場合はbody入力
+        let payload = String::new();
+        if http_ans.unwrap().eq("POST") {
+            let payload_text_key = Text::new("Please input an Payload key").prompt();
+            // エラーの場合はもう一度ループさせる
+            let payload_text_value: = Text::new("Please input an Payload value").prompt();
         }
 
         // Authorizationヘッダーの登録
@@ -53,13 +66,20 @@ impl Cmd {
         let filter_env_vars: Vec<String> = env_var.into_iter().filter(|x| x.0.contains("")).map(|x| x.0).collect();
         let select_var = Select::new("Please select an HTTP method.", filter_env_vars).prompt();
 
-        // postメソッドの場合はbody入力
         // fileデータを添付する場合
 
     }
 
 }
 
-fn input_host_name() {
+fn input_host_name() -> String {
     let host_name = Text::new("Please input Host name").prompt();
+    match host_name {
+        Ok(input) => return input,
+        Err(_) => std::process::exit(1)
+    }
+}
+
+impl CurlOption<'_> {
+    pub fn new() {}
 }
