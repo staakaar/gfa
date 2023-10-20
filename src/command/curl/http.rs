9dda58017e174b91f5e549378315942f8f27ad87
staakaar <vancyroletaka@gmail.com>
2023-10-20 22:55:18 +0900

    move post method impl

--- src/command/curl/http.rs
+++ src/command/curl/http.rs
@@ -1,4 +1,6 @@
-
+use std::collections::HashMap;
+use inquire::{InquireError, Text, required};
+use serde_json::{self, json};
 
 pub trait Http {
     fn exec(&self);
@@ -26,7 +28,34 @@ pub struct Post {}
 
 impl Http for Post {
     fn exec(&self) {
-        println!("http.rs POST method")
+        println!("http.rs POST method");
+        // postメソッドの場合はbody入力
+        let mut body_map: HashMap<String, String> = HashMap::new();
+
+        let payload_text: Result<String, InquireError>= Text::new("Please input an Payload key").with_validator(required!()).prompt();
+
+        let payload_list: Vec<&str> = match &payload_text {
+            Ok(text) => {
+                text.split_whitespace().collect::<Vec<&str>>()
+            }
+            Err(_) => {
+                std::process::exit(1);
+            }
+        };
+        
+        let mut payload_enum = payload_list.iter().enumerate();
+        while let Some((index, key)) = payload_enum.next() {
+            if let Some((index,value)) = payload_enum.next() {
+                body_map.insert(key.to_string(), value.to_string());
+            }
+        }
+
+        // payloadを作成していく
+        let payload = serde_json::to_string(&body_map).unwrap();
+        let payload_json = json!(payload);
+
+        let json_string = serde_json::to_string_pretty(&payload_json).unwrap();
+        print!("{}", json_string);
     }
 }
 
