a4d2fef4ab0ec80c3c3e91c6aac14b31e5029012
staakaar <vancyroletaka@gmail.com>
2023-11-09 22:45:27 +0900

    add curlOption impl

--- src/command/curl/options.rs
+++ src/command/curl/options.rs
@@ -1,26 +1,36 @@
 use std::collections::HashMap;
 
-struct CurlOption<'a> {
-    protocol: &'a str,
+#[derive(Debug)]
+pub struct CurlOption {
+    protocol: String,
     host_name: String,
     port: String,
-    http_method: &'a str,
+    http_method: String,
     authorization: String,
-    query_params: HashMap<&'a str, &'a str>,
+    query_params: HashMap<String, String>,
     body: String,
 }
 
 
-impl CurlOption<'_> {
-    pub fn new(&self) -> Self {
-        Self {
-            protocol: "",
+// pub trait CurlInput {
+//     fn new() -> CurlOption;
+// }
+
+impl CurlOption {
+    pub fn new() -> CurlOption {
+        CurlOption {
+            protocol: String::new(),
             host_name: String::new(),
             port: String::new(),
-            http_method: "",
+            http_method: String::new(),
             authorization: String::new(),
             query_params: HashMap::new(),
             body: String::new(),
         }
+    }
+
+    pub fn set_host_name(curl_option: &mut CurlOption, host: String) -> &mut CurlOption {
+        curl_option.host_name = host;
+        return curl_option;
     }
 }
