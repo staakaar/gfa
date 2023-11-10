2187809d90b7890f86eeaacd368c315d492e7083
staakaar <vancyroletaka@gmail.com>
2023-11-10 23:31:50 +0900

    add request get

--- src/command/curl/request.rs
+++ src/command/curl/request.rs
@@ -1,3 +1,5 @@
+
+
 pub trait Request {
     fn get(&self);
     fn post(&self);
@@ -5,8 +7,11 @@ pub trait Request {
     fn delete(&self);
 }
 
+#[tokio::main]
 impl Request {
-    fn get(&self) {}
+    fn get(&self) {
+        let res = reqwest::get().await?.json::<HashMap<String, String>>().await?;
+    }
 
     fn post(&self) {}
 
