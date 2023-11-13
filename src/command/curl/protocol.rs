d17c95ba637cf18441730a487e441363f6cb9d43
staakaar <vancyroletaka@gmail.com>
2023-11-13 23:24:01 +0900

    add cell trait

--- src/command/curl/protocol.rs
+++ src/command/curl/protocol.rs
@@ -1,26 +1,55 @@
+use std::cell::Cell;
+use std::collections::HashMap;
 use inquire::{InquireError, Select, Text};
-use crate::common;
+use crate::command::curl::authorization::{Authorization, Bearer};
+use crate::command::curl::headers::Header;
+use crate::command::curl::http::{HttpMethod, Get, Post, Put, Delete, Http};
+use crate::command::curl::params::Params;
+use crate::command::curl::curl_input::CurlInput;
+use crate::common::curl_config;
 
 pub trait Protocol {
-    fn exec(&self);
+    fn exec();
 }
 
-#[derive(PartialEq, Clone, Debug)]
+#[derive(Debug)]
 pub enum ProtocolType {
-    HTTP(HttpConn),
-    FILE(File),
+    HTTP,
+    FILE,
 }
 
-#[derive(PartialEq, Clone, Debug)]
-pub struct HttpConn {}
+
+pub struct HttpConn {
+    curl_option: Cell<CurlInput>
+}
+
+impl HttpConn {
+    fn init() -> HttpConn {
+        HttpConn { 
+            curl_option: Cell::new(
+                CurlInput { 
+                    protocol: String::new(),
+                    host_name: String::new(),
+                    port: String::new(),
+                    http_method: String::new(),
+                    authorization: String::new(),
+                    query_params: HashMap::new(),
+                    body: String::new(),
+                }
+            )
+        } 
+    }
+}
 
 impl Protocol for HttpConn {
-    fn exec(&self) {
-        // ホスト名の選択
-        let host_ans: Result<&str, InquireError> = Select::new("Please select an HOST name", common::curl_config::get_host()).prompt();
+    fn exec() {
+        let curl_input: HttpConn = HttpConn::init();
 
-        let mut host_text: String = "".to_owned();
-        if host_ans.unwrap() == "Other" {
+        // ホスト名の選択
+        let host_ans: Result<&str, InquireError> = Select::new("Please select an HOST name", curl_config::get_host()).prompt();
+
+        let mut host_text: String = host_ans.unwrap().to_string();
+        if host_text.eq("Other") {
             let host_text_ans: Result<String, InquireError> = Text::new("Please select an HOST name").prompt();
             match host_text_ans {
                 Ok(_) => {
@@ -33,6 +62,58 @@ impl Protocol for HttpConn {
 
         // ポート選択
         let port_ans: Result<String, InquireError> = Text::new("Please select an PORT name").prompt();
+        let port = port_ans.unwrap();
+
+        // HTTPメソッドの選択
+        let http_ans: Result<&str, InquireError> = Select::new("Please select an HTTP method.", curl_config::get_http_method()).prompt();
+        match http_ans {
+            Ok(choice) => println!("{}! That's mine too!", choice),
+            Err(_) => println!("There was an error, please try again"),
+        }
+
+        let http_type: HttpMethod = match http_ans.unwrap() {
+            "GET" => HttpMethod::GET,
+            "POST" => HttpMethod::POST,
+            "PUT" => HttpMethod::PUT,
+            "DELETE" => HttpMethod::DELETE,
+            _ => panic!("Please select an protocol name")
+        };
+
+        match http_type {
+            HttpMethod::GET => Get::exec(Get { curl_input }),
+            HttpMethod::POST => Post::exec(Post { curl_input }),
+            HttpMethod::PUT => Put::exec(Put { curl_input }),
+            HttpMethod::DELETE => Delete::exec(Delete { curl_input }),
+        };
+
+        // params有無
+        let is_params: Result<&str, InquireError> = Select::new("Set params?", curl_config::is_params()).prompt();
+        match is_params.unwrap() {
+            "YES" =>  Params::set(),
+            "NO" => println!("OK"),
+            _ => panic!("Please select yes or no"),
+        }
+
+        // header設定
+        let is_header: Result<&str, InquireError> = Select::new("Set params?", curl_config::is_params()).prompt();
+        match is_header.unwrap() {
+            "YES" =>  Header::set(),
+            "NO" => println!("OK"),
+            _ => panic!("Please select yes or no"),
+        }
+
+        // body設定
+
+        // Authorizationヘッダーの有無
+        let authorization_token = Select::new("Do you specify an Authorization header?", curl_config::get_authorization()).prompt();
+        match authorization_token.unwrap() {
+            "YES" =>  Authorization::check(&Bearer {}),
+            "NO" => println!("OK"),
+            _ => panic!("Please select yes or no"),
+        }
+
+        // set request data
+        // send request
     }
 }
 
@@ -48,7 +129,7 @@ fn input_host_name() -> String {
 pub struct File {}
 
 impl Protocol for File {
-    fn exec(&self) {
+    fn exec() {
         println!("fileが選択されました。")
     }
 }
