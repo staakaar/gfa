459b9a7a95eeb0f493b044a4bcb32518c51be0e9
staakaar <vancyroletaka@gmail.com>
2023-11-14 23:21:34 +0900

    select authorization type impl

--- src/command/curl/authorization.rs
+++ src/command/curl/authorization.rs
@@ -1,8 +1,10 @@
 use std::env;
 use inquire::{Select};
 
+use crate::common::curl_config;
+
 pub trait Authorization {
-    fn check(&self);
+    fn select(&self);
 }
 
 #[derive(PartialEq, Clone, Debug)]
@@ -13,10 +15,20 @@ pub enum AuthorizationType {
 #[derive(PartialEq, Clone, Debug)]
 pub struct Bearer {}
 
+impl dyn Authorization {}
+
 impl Authorization for Bearer {
-    fn check(&self) {
-        let env_var = env::vars();
-        let filter_env_vars: Vec<String> = env_var.into_iter().filter(|x| x.0.contains("")).map(|x| x.0).collect();
-        let select_var = Select::new("Please select an HTTP method.", filter_env_vars).prompt();
+    fn select(&self) {
+        // let env_var = env::vars();
+        // let filter_env_vars: Vec<String> = env_var.into_iter().filter(|x| x.0.contains("")).map(|x| x.0).collect();
+        // let select_var = Select::new("Please select an HTTP method.", filter_env_vars).prompt();
+        let select_var = Select::new("Please select an HTTP method.", curl_config::get_authorization_type()).prompt();
+        match select_var.unwrap() {
+            "No Auth" => println!("test"),
+            "API Key" => println!("test"),
+            "Bearer Token" => println!("test"),
+            "JWT Bearer" => println!("test"),
+            _ => std::process::exit(1)
+        }
     }
 }
